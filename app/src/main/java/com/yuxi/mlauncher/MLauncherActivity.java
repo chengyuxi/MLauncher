157cb8caa56906602bc302371e0f4166a972cd0c
chengyuxi <cheng.yuxi@tvsee.cn>
2015-06-11 01:38:58 +0800

    add launcher properties show apps installed

--- app/src/main/java/com/yuxi/mlauncher/MLauncherActivity.java
+++ app/src/main/java/com/yuxi/mlauncher/MLauncherActivity.java
@@ -1,17 +1,45 @@
 package com.yuxi.mlauncher;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 
-public class MLauncherActivity extends ActionBarActivity {
+public class MLauncherActivity extends Activity {
+    private GridView appList;
+    private List<ResolveInfo> apps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mlauncher);
+        initView();
+    }
+
+    public void loadApps(){
+        Intent intent = new Intent(Intent.ACTION_MAIN,null);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        new ImageView(this);
+
+        apps = getPackageManager().queryIntentActivities(intent,0);
+        appList.setAdapter(new AppAdapter());
+    }
+
+    private void initView() {
+        appList = (GridView) findViewById(R.id.app_list);
+        loadApps();
     }
 
     @Override
@@ -34,5 +62,35 @@ public class MLauncherActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class AppAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return apps.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return apps.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.layout_app_item,null);
+            ImageView icon = (ImageView) convertView.findViewById(R.id.app_icon);
+            TextView name = (TextView) convertView.findViewById(R.id.app_name);
+            ResolveInfo info = apps.get(position);
+            icon.setBackground(info.activityInfo.loadIcon(getPackageManager()));
+            name.setText(info.activityInfo.loadLabel(getPackageManager()));
+            return convertView;
+        }
+
     }
 }
